6e63d571ae7820550dc2eed19fe57fe19997a5ce
krisrogiers <rogierskrismail@gmail.com>
2015-03-30 13:05:58 +0200

    final

--- WpfButtonGebruik/ButtonGebruikWindow.xaml.cs
+++ WpfButtonGebruik/ButtonGebruikWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace WpfButtonGebruik
 {
@@ -23,19 +24,24 @@ namespace WpfButtonGebruik
         private void ButtonBold_Checked(object sender, RoutedEventArgs e)
         {
             LabelTekst.FontWeight = FontWeights.Bold;
+            CheckBoxBold.IsChecked = ButtonBold.IsChecked;
         }
 
         private void ButtonBold_Unchecked(object sender, RoutedEventArgs e)
         {
             LabelTekst.FontWeight = FontWeights.Normal;
+            CheckBoxBold.IsChecked=ButtonBold.IsChecked;
         }
 
         private void ButtonItalic_Click(object sender, RoutedEventArgs e)
         {
-            if (ButtonItalic.IsChecked == true)
+            ToggleButton knop = (ToggleButton)sender;
+            if (knop.IsChecked == true)
                 LabelTekst.FontStyle = FontStyles.Italic;
             else
                 LabelTekst.FontStyle = FontStyles.Normal;
+            CheckBoxItalic.IsChecked = knop.IsChecked;
+            ButtonItalic.IsChecked = knop.IsChecked;
 
         }
 
@@ -51,6 +57,26 @@ namespace WpfButtonGebruik
                 LabelTekst.FontSize--;
         }
 
+        private void Kleur_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton knop = (RadioButton)sender;
+            LabelTekst.Foreground=(SolidColorBrush) new BrushConverter().ConvertFromString(knop.Content.ToString());
+        }
 
+        private void CheckBoxBold_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBoxBold_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonBold.IsChecked = CheckBoxBold.IsChecked;
+        }
+
+
+
+
+
+ 
     }
 }
